18362ac54c1dd7a33dcbfd319a3e918730617891
Paulo Ruan <110623212+PauloRuan30@users.noreply.github.com>
2024-10-29 10:45:48 -0300

    Update ParallelSelectionSort.java

--- src/parallel/selection_sort/ParallelSelectionSort.java
+++ src/parallel/selection_sort/ParallelSelectionSort.java
@@ -1,11 +1,31 @@
-package parallel.selection_sort;
-import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ParallelSelectionSort {
-    private List<Integer> array;
 
-    public ParallelSelectionSort(List<Integer> array) {
-        this.array = array;
+    public static void parallelSelectionSort(int[] array, int numThreads) {
+        ExecutorService executor = Executors.newFixedThreadPool(numThreads);
+        int n = array.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            final int outerIndex = i;
+            executor.submit(() -> {
+                int currentMinIndex = outerIndex;
+                for (int j = outerIndex + 1; j < n; j++) {
+                    if (array[j] < array[currentMinIndex]) {
+                        currentMinIndex = j;
+                    }
+                }
+                swap(array, outerIndex, currentMinIndex);
+            });
+        }
+        executor.shutdown();
+        while (!executor.isTerminated()) {} // Wait for all tasks to finish
     }
-    
+
+    private static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
