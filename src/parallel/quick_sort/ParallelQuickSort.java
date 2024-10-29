3690194c5a646f438169c10841929319fed34750
Paulo Ruan <110623212+PauloRuan30@users.noreply.github.com>
2024-10-29 10:45:26 -0300

    Update ParallelQuickSort.java

--- src/parallel/quick_sort/ParallelQuickSort.java
+++ src/parallel/quick_sort/ParallelQuickSort.java
@@ -1,70 +1,50 @@
-package parallel.quick_sort;
 import java.util.concurrent.RecursiveAction;
 import java.util.concurrent.ForkJoinPool;
 
+public class ParallelQuickSort {
 
-
-public class ParallelQuickSort extends RecursiveAction{
-    private int[] array;
-    private int start;
-    private int end;
-    private int pivot;
-    private static final int THRESHOLD = 2;
-
-
-    public ParallelQuickSort(int[] array, int start, int end) {
-        this.array = array;
-        this.start = start;
-        this.end = end;
-        this.pivot = array[end];
+    public static void parallelQuickSort(int[] array, int numThreads) {
+        ForkJoinPool forkJoinPool = new ForkJoinPool(numThreads);
+        forkJoinPool.invoke(new QuickSortTask(array, 0, array.length - 1));
     }
 
-    @Override
-    protected void compute() {
-        if (end - start <= THRESHOLD) {
-            int i = start - 1;
-            for (int j = start; j < end; j++) {
+    private static class QuickSortTask extends RecursiveAction {
+        private int[] array;
+        private int low;
+        private int high;
+
+        public QuickSortTask(int[] array, int low, int high) {
+            this.array = array;
+            this.low = low;
+            this.high = high;
+        }
+
+        @Override
+        protected void compute() {
+            if (low < high) {
+                int pivotIndex = partition(array, low, high);
+                invokeAll(new QuickSortTask(array, low, pivotIndex - 1), new QuickSortTask(array, pivotIndex + 1, high));
+            }
+        }
+
+        private int partition(int[] array, int low, int high) {
+            int pivot = array[high];
+            int i = low - 1;
+
+            for (int j = low; j < high; j++) {
                 if (array[j] <= pivot) {
                     i++;
-                    int temp = array[j];
-                    array[j] = array[i];
-                    array[i] = temp;
+                    swap(array, i, j);
                 }
             }
-            int finalTemp = array[end];
-            array[end] = array[i + 1];
-            array[i + 1] = finalTemp;
-        } else {
-            int pivot = partition(array, end, start);
-            ParallelQuickSort leftTask = new ParallelQuickSort(array, start, pivot - 1);
-            ParallelQuickSort rightTask = new ParallelQuickSort(array, pivot + 1, end);
-            invokeAll(leftTask, rightTask); 
+            swap(array, i + 1, high);
+            return i + 1;
+        }
+
+        private void swap(int[] array, int i, int j) {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
         }
     }
-
-    private static int partition(int[] a, int high, int low) {
-        int pivot = a[high];
-        int i = low - 1;
-        for (int j = low; j < high; j++) {
-            if (a[j] <= pivot) {
-                i++;
-                int temp = a[j];
-                a[j] = a[i];
-                a[i] = temp;
-            }
-        }
-        int finalTemp = a[high];
-        a[high] = a[i + 1];
-        a[i + 1] = finalTemp;
-        return i + 1;
-    }
-
-    public static void main(String[] args) {
-        ForkJoinPool pool = new ForkJoinPool();
-        int[] array = {1, 3, 2, 0};
-        ParallelQuickSort task = new ParallelQuickSort(array, 0, array.length);
-        pool.invoke(task);
-        System.out.println(array);
-    }
-
 }
